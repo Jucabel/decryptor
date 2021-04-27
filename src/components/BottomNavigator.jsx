a25478d39aa4c5e6e3e7d1e91ff353f406950c1c
EmmaProject <emma.project.development@gmail.com>
2021-04-26 23:42:52 -0500

    BottomNavigator was added and linked with the store

--- src/components/BottomNavigator.jsx
+++ src/components/BottomNavigator.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import QRReader from './QRReader';
+import QRList from './QRList';
+
+const Tab = createMaterialBottomTabNavigator();
+const STYLES = ['default', 'dark-content', 'light-content'];
+const TRANSITIONS = ['fade', 'slide', 'none'];
 
 const BottomNavigator = () => {
+  const [hidden, setHidden] = useState(false);
+  const [statusBarStyle, setStatusBarStyle] = useState(STYLES[2]);
+  const [statusBarTransition, setStatusBarTransition] = useState(TRANSITIONS[0]);
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator barStyle={{ backgroundColor: '#2c1352' }}>
+        <Tab.Screen
+          name='READ'
+          component={QRReader}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name='qrcode-plus' color={color} size={22} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='LIST'
+          component={QRList}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name='format-list-checkbox' color={color} size={22} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
